4cec1e286ec4bdf86500b610a0ec481e4bd2c919
Qadir Hussain <qadir.hussain99@gmail.com>
2022-05-10 06:52:55 +0100

    Scala object oriented tutorials

--- src/main/scala/org/techieguy/ObjectOrientation.scala
+++ src/main/scala/org/techieguy/ObjectOrientation.scala
@@ -1,5 +1,25 @@
 package org.techieguy
 
-object ObjectOrientation {
+object ObjectOrientation extends App {
+
+  // class and instance
+  class Animal {
+    // define fields
+    val age: Int = 0
+    def eat() = println("Love eating!")
+  }
+  val anAnimal = new Animal
+
+  // inheritance
+  class Dog(val name: String) extends Animal { // constructor definition
+
+  }
+  val aDog = new Dog("Alice")
+  // constructor arguments are NOT fields: need to put a val before the constructor argument
+  print(aDog.name)
+
+  // subtype polymorphism
+  val aDeclaredAnimal: Animal = new Dog("Helio")
+  aDeclaredAnimal.eat() // the most derived method will be called at runtime
 
 }
