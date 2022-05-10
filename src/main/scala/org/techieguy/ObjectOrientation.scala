8958d0ebc7c54591f02a2a96a607b5bbf8e86bd3
Qadir Hussain <qadir.hussain99@gmail.com>
2022-05-10 07:04:16 +0100

    Scala object oriented tutorials - adding class, abstract, trait with inheritance

--- src/main/scala/org/techieguy/ObjectOrientation.scala
+++ src/main/scala/org/techieguy/ObjectOrientation.scala
@@ -5,7 +5,7 @@ object ObjectOrientation extends App {
   // class and instance
   class Animal {
     // define fields
-    val age: Int = 0
+    val age: Int = 0 // public by default, can restrict by adding protected or private
     def eat() = println("Love eating!")
   }
   val anAnimal = new Animal
@@ -21,5 +21,20 @@ object ObjectOrientation extends App {
   // subtype polymorphism
   val aDeclaredAnimal: Animal = new Dog("Helio")
   aDeclaredAnimal.eat() // the most derived method will be called at runtime
+
+  // abstract class
+  abstract class WalkingAnimal {
+    val hasLegs = true
+    def walk(): Unit
+  }
+
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  // single-class inheritance and multi-trait ("mixing")
+  class Lion extends Animal with Carnivore {
+    override def eat(animal: Animal): Unit = println("I'm eating don't bother me!")
+  }
 
 }
