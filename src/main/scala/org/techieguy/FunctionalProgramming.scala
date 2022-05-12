ebaeeb1d76353ce0f456260116506c71e681b5f0
Qadir Hussain <qadir.hussain99@gmail.com>
2022-05-12 21:22:36 +0100

    Scala object oriented tutorials - Scala Functional Programming

--- src/main/scala/org/techieguy/FunctionalProgramming.scala
+++ src/main/scala/org/techieguy/FunctionalProgramming.scala
@@ -2,5 +2,42 @@ package org.techieguy
 
 object FunctionalProgramming extends App {
 
+  // Scala is Object Oriented
+  class Person(name: String) {
+    def apply(age: Int): Unit = println("Scala is object oriented language")
+  }
+
+  val bob = new Person("Bob")
+  bob.apply(20)
+  bob(20) // INVOKING bob as a function === bob.apply(20)
+
+  /*
+  * Scala runs on JVM
+  * Functional Programming: To make Scala functional aspect into object, Scala treats every method as first class citizen
+  * - Compose functions
+  * - Pass functions as arguments
+  * - Return functions as results
+  *
+  * Conclusion: FunctionX = Function1, Function2, ... Function22
+  * */
+
+  val simpleIncrementor = new Function1[Int, Int] {
+    override def apply(val1: Int): Int = val1 + 1
+  }
+  simpleIncrementor.apply(25) // Output 26
+  simpleIncrementor(25)
+
+  // Sugar syntax
+  val doubler1: Int => Int = (x: Int) => x + 10
+  val doubler: Function1[Int, Int] = (x: Int) => x + 10 //Can be written as -> val doubler = (x: Int) => x.+(10)
+  doubler(10) // Output: 20
+
+  /*
+  * Equivalent to much larger code below
+  *
+  * val doubler: Function1[Int, Int] = new Function1[Int, Int] {
+  *   override def apply(x: Int): Int = x + 10
+  * }
+  * */
 
 }
