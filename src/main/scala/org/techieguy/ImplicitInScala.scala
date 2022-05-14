f66194b475d70b06a7295038ae46b016183814f1
Qadir Hussain <qadir.hussain99@gmail.com>
2022-05-14 12:59:43 +0100

    Scala object oriented tutorials - Scala Implicit uses

---
+++ src/main/scala/org/techieguy/ImplicitInScala.scala
@@ -0,0 +1,21 @@
+package org.techieguy
+
+object ImplicitInScala extends App {
+
+  /**
+   * Implicits basics
+   * */
+
+  // implicit arguments
+  def aMethodWithImplicitArgs(implicit num: Int) = num + 10
+  implicit val aNum = 10
+  println(aMethodWithImplicitArgs)  // aMethodWithImplicitArgs(aNum)
+
+  // implicit conversions
+  implicit class IntegerWrapper(num: Int) {
+    def isEven(): Boolean = num % 2 == 0
+  }
+  println(23.isEven())  // new IntegerWrapper(23).isEven()
+
+  //NOTE: use it carefully
+}
